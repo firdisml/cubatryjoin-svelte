07dd0d30f3df59701dcbc48da9bc99ce8ee45cd5
Firdaus Ismail <44713893+firdisml@users.noreply.github.com>
2023-03-05 01:56:48 +0800

    Added datepicker

--- src/routes/submit/+page.svelte
+++ src/routes/submit/+page.svelte
@@ -1,10 +1,13 @@
-<script>
-    
+<script lang="ts">
+	import dayjs from 'dayjs';
+	import { Datepicker } from 'svelte-calendar';
+
+	let store:any;
+
+
 </script>
 
-<style>
-    
-</style>
+
 <div class="px-4 py-4 sm:px-0">
     <div class="gap-2 text-md border-2 border-black bg-pink-200 font-semibold shadow-[3px_3px_0_0_#000] overflow-visible mt-5">
 
@@ -63,6 +66,28 @@
                     class="mt-2 block text-black py-2.5 px-3 w-full text-md font-normal border-2 border-black focus:outline-none focus:ring-black shadow-[3px_3px_0_0_#000]"
                 />
             </div>
+
+            
+            <label
+                for="link"
+                class="flex text-black text-md font-bold -mb-3"
+            >
+                Start Date
+            </label>
+            
+            <Datepicker bind:store let:key let:send let:receive>
+                <button type="button" class="flex mb-1 items-center justify-center gap-2 text-md  border-2 border-black bg-white px-4 py-2 font-semibold shadow-[3px_3px_0_0_#000] transition hover:shadow-none focus:outline-none active:bg-white" in:receive|local={{ key }} out:send|local={{ key }}>
+                    {#if $store?.hasChosen}
+                        {dayjs($store.selected).format('ddd MMM D, YYYY')}
+                    {:else}
+                        Pick a Date
+                    {/if}
+                </button>
+            </Datepicker>
+
+        
+
+
     </div>
 </div>
 
