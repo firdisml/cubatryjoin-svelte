6f013ce35c674fc0f570a3b0b3949fab4d043eeb
Firdaus Ismail <44713893+firdisml@users.noreply.github.com>
2023-03-04 22:56:15 +0800

    Fix order button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -45,7 +45,7 @@
 		viewBox="0 0 24 24"
 		stroke-width="1.5"
 		stroke="currentColor"
-		class="w-5 h-5 "
+		class="w-5 h-5 rotate-180"
 	>
 		<path
 			stroke-linecap="round"
@@ -60,7 +60,7 @@
 		viewBox="0 0 24 24"
 		stroke-width="1.5"
 		stroke="currentColor"
-		class="w-5 h-5 rotate-180"
+		class="w-5 h-5"
 	>
 		<path
 			stroke-linecap="round"
